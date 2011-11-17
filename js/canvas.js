d2abdebdac1efb1f00a8197272f936cb889aaed6
Felipe Norato Lacerda <fnoratol@gmail.com>
2011-11-17 20:48:17 -0200

    add the dimensions's board

--- js/canvas.js
+++ js/canvas.js
@@ -4,11 +4,12 @@ pedalsIV = $("#pedalsIV");
 pedalsVI = $("#pedalsVI");
 pedalsVIII = $("#pedalsVII");
 pedalsX = $("#pedalsX");
-pedalsCustom = $("#pedalCustom");
+pedalsCustom = $("#pedalsCustom");
 
 board = $("#board");
 mxr = $("#MXR");
 boss = $("#Boss");
+rocktron = $("#Rocktron");
 
 limit = $("#limit");
 
@@ -33,9 +34,13 @@ function controlPedalboarPosition(){
     var board_top = ((limit_height/2) - board_height/2);
     var board_left = ((limit_width/2) - board_width/2);
     board.css({'top':board_top+'px', 'left':board_left+'px'});
-
+    limit.find("#width").html((board_width/10)+'cm')
+        .css({'top':(limit_height-board_top),
+              'left':(board_left+(board_width/2))});
+    limit.find("#height").html((board_height/10)+'cm')
+        .css({'top':(board_top+(board_height/2)),
+              'left':(limit_width- board_left)});
 };
-
 
 //------ events----- //
 // 1pedal width=90 height=150
@@ -43,10 +48,11 @@ pedalsIV.click(function(){create(360,150)});
 pedalsVI.click(function(){create(540,150)});
 pedalsVIII.click(function(){create(360,300)});
 pedalsX.click(function(){create(450,300)});
-pedalsCustom.click(function(){create(200,100)});
+pedalsCustom.click(function(){create(600,400)});
 
 mxr.click(function(){createped('mxr')});
 boss.click(function(){createped('boss')});
+rocktron.click(function(){createped('rocktron')});
 
 
 });
