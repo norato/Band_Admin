8b79a4b1ed1cc1e4f0a882495e58371d8070149b
norato <fnoratol@gmail.com>
2013-04-21 14:04:56 -0300

    Adição de Proprietário em Local #8

--- spec/acceptance/gerenciar_local_spec.rb
+++ spec/acceptance/gerenciar_local_spec.rb
@@ -3,39 +3,72 @@
 require 'spec_helper'
 
 feature 'Gerenciar Locais' do
-	def preencher_e_verificar_local
-		fill_in 'Nome',     :with => "Manifesto" 
-		fill_in 'Endereço', :with => "R. Iguatemi, 36C - Itaim Bibi"
-  	fill_in 'Cidade',   :with => "São Paulo"
-  	fill_in 'Tipo',     :with => "Bar"
-  	
-  	click_button 'Salvar'
+  scenario 'adicionar local' do
+    proprietario = FactoryGirl.create(:proprietario) 
+    
+    visit new_local_path
+    
+    fill_in 'Nome',     :with => "Manifesto" 
+    fill_in 'Endereço', :with => "R. Iguatemi, 36C - Itaim Bibi"
+    fill_in 'Cidade',   :with => "São Paulo"
+    fill_in 'Tipo',     :with => "Bar"
 
-		page.should have_content "Nome: Manifesto" 
-		page.should have_content "Endereço: R. Iguatemi, 36C - Itaim Bibi"
-  	page.should have_content "Cidade: São Paulo"
-  	page.should have_content "Tipo: Bar"
-	end
-	
-	scenario 'adicionar local' do
-		visit new_local_path
-		preencher_e_verificar_local
-	end
+    select "#{proprietario.nome}", from: 'Proprietário'
+    
+    click_button 'Salvar'
 
-	scenario 'editar local' do
-		local = FactoryGirl.create(:local)
-		visit edit_local_path(local)
-		preencher_e_verificar_local
-	end
 
-	scenario 'deletar local' do
-		local = FactoryGirl.create(:local)
+    page.should have_content "Nome: Manifesto" 
+    page.should have_content "Endereço: R. Iguatemi, 36C - Itaim Bibi"
+    page.should have_content "Cidade: São Paulo"
+    page.should have_content "Tipo: Bar"
+    page.should have_content "Proprietário: #{proprietario.nome}"
+  end
 
-		visit locais_path
-		click_link 'Excluir'
-		page.should_not have_content "Nome: Manifesto" 
-		page.should_not have_content "Endereço: R. Iguatemi, 36C - Itaim Bibi"
-  	page.should_not have_content "Cidade: São Paulo"
-  	page.should_not have_content "Tipo: Bar"
+  scenario 'editar local' do
+    local = FactoryGirl.create(:local)
+    visit edit_local_path(local)
+    proprietario = FactoryGirl.create(:proprietario) 
+    
+    visit new_local_path
+    
+    fill_in 'Nome',     :with => "Manifesto" 
+    fill_in 'Endereço', :with => "R. Iguatemi, 36C - Itaim Bibi"
+    fill_in 'Cidade',   :with => "São Paulo"
+    fill_in 'Tipo',     :with => "Bar"
+
+    select "#{proprietario.nome}", from: 'Proprietário'
+    
+    click_button 'Salvar'
+
+
+    page.should have_content "Nome: Manifesto" 
+    page.should have_content "Endereço: R. Iguatemi, 36C - Itaim Bibi"
+    page.should have_content "Cidade: São Paulo"
+    page.should have_content "Tipo: Bar"
+    page.should have_content "Proprietário: #{proprietario.nome}"
+  end
+
+  scenario 'deletar local' do
+    proprietario = FactoryGirl.create(:proprietario) 
+    
+    visit new_local_path
+    
+    fill_in 'Nome',     :with => "Manifesto" 
+    fill_in 'Endereço', :with => "R. Iguatemi, 36C - Itaim Bibi"
+    fill_in 'Cidade',   :with => "São Paulo"
+    fill_in 'Tipo',     :with => "Bar"
+
+    select "#{proprietario.nome}", from: 'Proprietário'
+    
+    click_button 'Salvar'
+
+    visit locais_path
+
+    click_link 'Excluir'
+    page.should_not have_content "Nome: Manifesto" 
+    page.should_not have_content "Endereço: R. Iguatemi, 36C - Itaim Bibi"
+    page.should_not have_content "Cidade: São Paulo"
+    page.should_not have_content "Tipo: Bar"
   end
 end
